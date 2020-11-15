353f97a4989e275cc3aa321b1ae288d1fbd87acb
Archit Kaul <architkl@gmail.com>
2020-11-15 22:05:30 +0530

    Code cleanup

---
+++ internal/checkSameChannel.go
@@ -0,0 +1,47 @@
+package internal
+
+import (
+	"../framework"
+	"errors"
+)
+
+// Check if user and bot are in the same voice channel
+func CheckSameChannel(ctx framework.Context) error {
+
+	Logging(ctx)
+
+	// Check if connected
+	if !ctx.MediaPlayer.IsConnected {
+		ctx.Reply("Bot not connected!")
+		return errors.New("Player not connected")
+	}
+
+	// Look for the message sender in that guild's current voice states.
+	var userID, botID, userVoiceChannelID, botVoiceChannelID string
+	userID = ctx.Message.Author.ID
+
+	if usr, err := ctx.Discord.User("@me"); err != nil {
+		ctx.Reply("Error")
+		return errors.New("Error obtaining account details")
+	} else {
+		botID = usr.ID
+	}
+
+	// Get voice channels for user and bot
+	for _, vs := range ctx.Guild.VoiceStates {
+		if vs.UserID == userID {
+			userVoiceChannelID = vs.ChannelID
+		}
+
+		if vs.UserID == botID {
+			botVoiceChannelID = vs.ChannelID
+		}
+	}
+
+	if userVoiceChannelID != botVoiceChannelID {
+		ctx.Reply("You're not in my voice channel!")
+		return errors.New("User and Bot not in same channel")
+	}
+
+	return nil
+}
