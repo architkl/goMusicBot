db376ffc489327beea5a5df895ba6691a1fc1e0e
Archit Kaul <architkl@gmail.com>
2020-11-25 20:56:30 +0530

    Show commands and description

---
+++ internal/help.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"../framework"
+	"sort"
+	"strings"
+)
+
+func Help(ctx framework.Context) {
+
+	var desc []string
+	for k, v := range ctx.CmdHandler.GetCmds() {
+		desc = append(desc, "+"+k+":"+strings.Repeat(" ", 9-len(k))+v.GetHelp())
+	}
+	sort.Strings(desc)
+
+	ctx.ReplyEmbed("Help", "```ml\n"+strings.Join(desc, "\n")+"```", 0x228FD3)
+}
